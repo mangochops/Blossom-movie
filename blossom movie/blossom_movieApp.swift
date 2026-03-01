0a4977c70a20241f34182766c8e949273ee727ba
mangochops <whymangoeswillicent@gmail.com>
2026-03-01 17:26:33 +0300

    Initial Commit

---
+++ blossom movie/blossom_movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  blossom_movieApp.swift
+//  blossom movie
+//
+//  Created by mac on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct blossom_movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
