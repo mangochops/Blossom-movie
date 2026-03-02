30a3e0a0496c225bddac179d233ddfe7605f7782
mangochops <whymangoeswillicent@gmail.com>
2026-03-02 11:54:00 +0300

    UI:Adding a horizontel view section

---
+++ HorizontalListView.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalListView.swift
+//  blossom movie
+//
+//  Created by mac on 3/2/26.
+//
+
+import SwiftUI
+
+struct HorizontalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalListView()
+}
